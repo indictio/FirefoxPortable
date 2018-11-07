3712b9b305eaa4050730ea1131023027efcbef6d
gabi <gabriel.breahna@euro.ro>
2018-11-07 18:51:34 +0200

    inno install

--- install.bat
+++
@@ -1,3 +0,0 @@
-setx moz_plugin_path "C:\PortableApps\FirefoxPortable\Data\plugins;C:\PortableApps\CommonFiles\Java\bin\plugin2"
-setx path "C:\PortableApps\FirefoxPortable;%path%"
-C:\PortableApps\FirefoxPortable\FirefoxPortable.exe
