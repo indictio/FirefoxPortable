842a87aba97f3244031c758cba526017cd7bdb97
gabosu <gabriel.breahna@euro.ro>
2018-12-06 18:07:09 +0200

    discoverer

--- tools/JInit initial/jinit/custcert.bat
+++
@@ -1,3 +0,0 @@
-C:\PROGRA~1\Oracle\JINITI~1.16\bin\javakey.exe r appltop
-C:\PROGRA~1\Oracle\JINITI~1.16\bin\javakey.exe c appltop true
-C:\PROGRA~1\Oracle\JINITI~1.16\bin\javakey.exe ic appltop appltop.cer
